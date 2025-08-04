a7b0714b9b145d74cbe8f6bd9051c7b255dfdbfe
Peta Gnaneshwar Reddy <petagnaneshwarreddy@gmail.com>
2025-08-04 12:02:34 +0530

    Updated code with new features/changes

--- src/Crud.java
+++ src/Crud.java
@@ -1,9 +1,16 @@
+import java.sql.*;
 public class Crud {
-
     public static void main(String[] args) {
         try {
-            
-        } catch (Exception e) {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mec", "root", "Peta@00");
+            if (con != null) {
+                System.out.println("Connected Successfully");
+                con.close();
+            } else {
+                System.out.println("Failed to connect");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
         }
     }
 }
