3af447c1b5d95af860861d2cca2e8eabdcad4c2e
Peta Gnaneshwar Reddy <petagnaneshwarreddy@gmail.com>
2025-08-04 12:58:10 +0530

    Updated code with new features/changes

--- src/Crud.java
+++ src/Crud.java
@@ -1,15 +1,35 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 public class Crud {
     public static void main(String[] args) {
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mec", "root", "Peta@00");
             if (con != null) {
                 System.out.println("Connected Successfully");
+                int Roll_no = 1; 
+                String Name = "Peta Gnaneshwar Reddy";
+                String Department = "Computer Science";
+                int Year = 3;
+                String Gender = "Male"; 
+                Date Date_info = Date.valueOf("2024-06-10"); 
+                String insert = "INSERT INTO STUDENT(Roll_no, Name, Department, Year, Gender, Date_info) VALUES (?, ?, ?, ?, ?, ?)";
+                PreparedStatement psInsert = con.prepareStatement(insert);
+                psInsert.setInt(1, Roll_no);
+                psInsert.setString(2, Name);
+                psInsert.setString(3, Department);
+                psInsert.setInt(4, Year);
+                psInsert.setString(5, Gender);
+                psInsert.setDate(6, Date_info); 
+                int rows = psInsert.executeUpdate();
+                System.out.println("Rows inserted: " + rows);
+                psInsert.close();
                 con.close();
             } else {
                 System.out.println("Failed to connect");
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
